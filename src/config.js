073b16b6cbd9eeecfa60e762f468f05e7a74a2b9
qqq2830 <qqq2830@163.com>
2018-11-25 23:47:20 +0800

    init

---
+++ src/config.js
@@ -0,0 +1,11 @@
+// export const API_ROOT = (process.env.NODE_ENV === 'dev')
+//   ? 'https://api.jackhu.top/'
+//   :'http://localhost:9000/'
+
+export const API_ROOT =  'https://api.jackhu.top/'
+
+// export const CookieDomain = (process.env.NODE_ENV === 'dev')
+//   ? '.jackhu.top'
+//   : ''
+
+export const CookieDomain = '.jackhu.top'
